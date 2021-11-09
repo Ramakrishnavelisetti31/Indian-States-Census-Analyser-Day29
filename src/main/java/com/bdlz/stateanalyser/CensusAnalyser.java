0adb39611caf16c73e0deb02c67b07b049cf85d3
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-09 22:47:17 +0530

    Added : Welcome message

---
+++ src/main/java/com/bdlz/stateanalyser/CensusAnalyser.java
@@ -0,0 +1,7 @@
+package com.bdlz.stateanalyser;
+
+public class CensusAnalyser {
+    public static void main(String[] args) {
+        System.out.println("welcome to Indian state analyser program");
+    }
+}
