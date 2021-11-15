97a9f7854fe268525e998ed487f165b3e9f282c5
ramakrishnavelisetti <ramakrishna96.velisetti@gmail.com>
2021-11-15 13:31:25 +0530

    Added: Given indian states census file returning number of records count

--- src/test/java/com/bdlz/stateanalyser/CensusAnalyserTest.java
+++ src/test/java/com/bdlz/stateanalyser/CensusAnalyserTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.*;
 public class CensusAnalyserTest {
     private String INDIAN_CENSUS_CSV_FILE_PATH = "C:\\Users\\lenovo\\IdeaProjects\\IndianStateAnalyser\\src\\main\\resources\\IndianStateCensus.csv";
 
+    // Tc1.1
     @Test
     public void givenIndianCensusCSVFile_WhenLoad_ShouldReturnCorrectRecords() {
         CensusAnalyser censusAnalyser = new CensusAnalyser();
